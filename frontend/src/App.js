25e728fc8b03088eb6d13f97d6023b78fec9752c
DaytonS3 <Daytonrsteinmeyer@gmail.com>
2019-05-09 19:08:49 -0400

    MVP COMPLETE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route, NavLink, withRouter } from "react-router-dom";
+import Login from "./components/Login";
+import Users from "./components/Users";
 
-function App() {
+function App(props) {
+  function logout() {
+    localStorage.removeItem("token");
+    props.history.push("/login");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>User App</h2>
+      <NavLink to="/login">Login</NavLink>
+      <NavLink to="/users">Users</NavLink>
+      <button type="button" onClick={logout}>
+        Logout
+      </button>
+      <Route path="/login" component={Login} />
+      <Route path="/users" component={Users} />
     </div>
   );
 }
 
-export default App;
+export default withRouter(App);
